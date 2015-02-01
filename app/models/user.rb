bb3a236e31c3294ac7a794005b381c7cc86cd1ed
federico tagliabue <fede.tagliabue@gmail.com>
2015-02-01 20:45:33 +1100

    user confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   # :confirmable
   
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable,
          :omniauthable, :omniauth_providers => [:facebook]
 
 
