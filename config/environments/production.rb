739c33c7d4b76002c7a83b8bd540df9c8319333a
federico tagliabue <fede.tagliabue@gmail.com>
2015-02-01 20:45:14 +1100

    add email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+    # Development Mailer Account
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => 'google.com',
+    :user_name => 'mytestapp84@gmail.com',
+    :password => '923djiwedjo39r',
+    :authentication => 'plain',
+    :enable_starttls_auto => true
+  } 
 end
