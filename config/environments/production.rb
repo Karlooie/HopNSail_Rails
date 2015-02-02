9cfc7c20943854b315f553b0177b16209b6adcb0
federico tagliabue <fede.tagliabue@gmail.com>
2015-02-02 20:23:27 +1100

    email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
     :port => 587,
-    :domain => 'mytestapp84@gmail.com',
+    :domain => 'gmail.com',
     :user_name => 'mytestapp84@gmail.com',
     :password => '923djiwedjo39r',
     :authentication => 'plain',
