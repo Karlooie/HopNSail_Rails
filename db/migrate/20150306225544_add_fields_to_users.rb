e3a77669ec2775148ac7bb4d3d52699171451c76
federico tagliabue <fede.tagliabue@gmail.com>
2015-03-07 11:33:35 +1100

    fix migration

--- db/migrate/20150306225544_add_fields_to_users.rb
+++ db/migrate/20150306225544_add_fields_to_users.rb
@@ -1,12 +1,8 @@
 class AddFieldsToUsers < ActiveRecord::Migration
-  def up
+
+  def change
     add_column :users, :birthday, :date
     add_column :users, :fb_id, :string
-  end
-
-  def down
-    remove_column :users, :birth_date, :date
-    remove_column :users, :fb_id, :string
   end
 
 end
