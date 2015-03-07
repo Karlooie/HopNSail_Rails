ecac4b0ad4f60dc5052f7bfe60523de09ca90dc7
federico tagliabue <fede.tagliabue@gmail.com>
2015-03-07 11:22:32 +1100

    change migration for heroku

--- db/migrate/20150306225544_add_fields_to_users.rb
+++ db/migrate/20150306225544_add_fields_to_users.rb
@@ -1,6 +1,12 @@
 class AddFieldsToUsers < ActiveRecord::Migration
-  def change
+  def up
     add_column :users, :birthday, :date
     add_column :users, :fb_id, :string
   end
+
+  def down
+    add_column :users, :birth_date, :date
+    add_column :users, :fb_id, :string
+  end
+
 end
